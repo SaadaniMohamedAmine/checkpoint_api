53cfac8a59f2a418ed1232e32a2c70be502426fb
Saadani Mohmaed Amine <saadanimohamedamine@gmail.com>
2020-10-26 22:54:48 +0100

    fetching list of users from API endpoint

--- src/UserList.js
+++ src/UserList.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
+import axios from 'axios'
 
 const UserList = () => {
+    const[listOfUSer,setListOfUSer]=useState([]) ;
+    useEffect(()=>{
+        axios
+          .get('https://jsonplaceholder.typicode.com/users')
+          .then((res)=>{
+              setListOfUSer(res.data) ;
+            }) 
+          .catch((err)=>{console.log(err)}) 
+
+    },[])
+    
     return (
         <div>
-            <h3>The user list !</h3>
+             <h4>List of users</h4>
+             {listOfUSer.map((user)=>(
+                 <div>
+                     {user.name}
+                 </div>
+             ))}
         </div>
     )
 }
